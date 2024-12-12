77708067b07cacb0bde1d00b6cfa5bc8710b7d3e
Georgi Malkhasyan (CW) <gmalkhasyan@paypal.com>
2024-12-12 14:20:03 -0800

    Initial Commit

---
+++ paypal-ios-sdk-demo-app/paypal_ios_sdk_demo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  paypal_ios_sdk_demo_appApp.swift
+//  paypal-ios-sdk-demo-app
+//
+//  Created by Georgi Malkhasyan on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct paypal_ios_sdk_demo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ paypal-ios-sdk-demo-appUITests/paypal_ios_sdk_demo_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  paypal_ios_sdk_demo_appUITestsLaunchTests.swift
+//  paypal-ios-sdk-demo-appUITests
+//
+//  Created by Georgi Malkhasyan on 12/12/24.
+//
+
+import XCTest
+
+final class paypal_ios_sdk_demo_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
